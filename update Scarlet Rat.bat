cc5fb61fa6eb1a24a5d9a59ce56df3c0303c3f7a
Muxite <muk.chunpongtong@gmail.com>
2024-04-06 18:53:00 -0600

    autoCommit 2024 0Sa.185300

--- update Scarlet Rat.bat
+++ update Scarlet Rat.bat
@@ -1,4 +1,4 @@
-start 
+@echo off
 set CONDA_ENV=SeleniumBot
 call conda activate %CONDA_ENV%
 python "D:\Github\muxite.github.io\scarlet_rat_v0.py"
@@ -8,4 +8,4 @@ cd D:\Github\muxite.github.io
 git add --all
 git commit -m "autoCommit %date:~-4%%date:~3,2%%date:~0,2%.%time:~0,2%%time:~3,2%%time:~6,2%"
 git push
-exit 0
+exit

2a51b6014163e0703a6d5387cc81c24150e63b52
Muxite <muk.chunpongtong@gmail.com>
2024-05-31 17:25:27 -0600

    created win_check function
    the function checks if a update to the board results in a victory. optimizations have been made to keep it performing well, since the computer opponent will use win_check frequently

--- tictactoe.py
+++ tictactoe.py
@@ -3,55 +3,55 @@ line_length = 3
 board = [[0]*board_width]*board_height
 
 
-def win_check (test_board, updated_square, required_score):
-    check_x, check_y = updated_square
-
-
-def square_check (board_w, board_h, test_board, updated_square, required_score, current_score, vector, parts):
-
+def win_check(board_w, board_h, test_board, updated_square, required_score):
+    looking_for = test_board[updated_square[1]][updated_square[0]]
     neighbors = []
-    if vector is None:
-        # get a list of neighbors of starting square
-        for xs in [-1, 0, 1]:
-            for ys in [-1, 0, 1]:
-                if (xs, ys) != (0, 0):
-                    if updated_square[0]-1 < 0 and ys == -1:  # vertical 0 edge
-                        continue
-                    elif updated_square[0]+1 > board_h-1 and ys == 1:  # high edge
-                        continue
-                    if updated_square[1]-1 < 0 and xs == -1:  # horizontal 0 edge
-                        continue
-                    elif updated_square[1]+1 > board_w-1 and xs == 1:  # horizontal limit edge
-                        continue
-                    # otherwise add the square to neighbours[]
-                    neighbors.append((updated_square[1] + xs, updated_square[0] + ys))
-                else:
+    # get a list of neighbors of starting square
+    for xs in [-1, 0, 1]:
+        for ys in [-1, 0, 1]:
+            if (xs, ys) != (0, 0):
+                if updated_square[0]-1 < 0 and ys == -1:  # vertical 0 edge
                     continue
-    else:
-        if vector == (1, 0) or vector == (-1, 0):
-            neighbors.append((updated_square[1] + 1, updated_square[0]))
-            neighbors.append((updated_square[1] - 1, updated_square[0]))
-        elif vector == (0, 1) or vector == (0, 1):
-            neighbors.append((updated_square[1], updated_square[0] - 1))
-            neighbors.append((updated_square[1], updated_square[0] + 1))
-        elif vector == (1, 1) or vector == (-1, -1):
-            neighbors.append((updated_square[1] - 1, updated_square[0] - 1))
-            neighbors.append((updated_square[1] + 1, updated_square[0] + 1))
-        elif vector == (1, -1) or vector == (-1, 1):
-            neighbors.append((updated_square[1] + 1, updated_square[0] - 1))
-            neighbors.append((updated_square[1] - 1, updated_square[0] + 1))
-
-    updated_square_value = test_board[updated_square[1]][updated_square[0]]
-    #  go through each neighbor to see if they are the same as current
-    current_score = len(parts)
-    for neighbor in neighbors:
-        #
-        if test_board[neighbor[1]][neighbor[0]] == updated_square_value:
-            if current_score+1 == required_score:  # if the length has been reached
-                return True, updated_square  # win for this value
+                elif updated_square[0]+1 > board_h-1 and ys == 1:  # high edge
+                    continue
+                if updated_square[1]-1 < 0 and xs == -1:  # horizontal 0 edge
+                    continue
+                elif updated_square[1]+1 > board_w-1 and xs == 1:  # horizontal limit edge
+                    continue
+                # otherwise add the square to neighbours[]
+                neighbors.append((updated_square[1] + xs, updated_square[0] + ys))
             else:
-                #  check the ones in line of the vector (front and behind)
+                continue
 
+    for neighbor in neighbors:
+        vector = (updated_square[0]-neighbor[0],updated_square[1]-neighbor[1])  # ex (1,0), (-1,-1)
+        current_score = 2  # the updated square is 1, the neighbor in this direction is 2
+        # keep checking next square in that direction until win
+        for i in range(required_score):
+            if current_score > required_score:
+                print("WINNER" + str(looking_for))
+                return True, looking_for  # did someone win? if so, who?
+            else:
+                # if the next square in the vector is same as looking_for
+                try:  # 2 is added to skip to after neighbor
+                    if test_board[updated_square[1]+(i+2)*vector[1]][updated_square[0]+(i+2)*vector[0]] == looking_for:
+                        current_score += 1
+                except IndexError:  # out of bounds
+                    break  # now
+        # now check reverse direction
+        for i in range(required_score):
+            if current_score > required_score:
+                print("WINNER" + str(looking_for))
+                return True, looking_for  # did someone win? if so, who?
+            else:
+                # if the next square in the vector is same as looking_for
+                try:  # add 1 to skip the starting block
+                    if test_board[updated_square[1] + (i+1) * -vector[1]][
+                     updated_square[0] + (i+1) * -vector[0]] == looking_for:
+                        current_score += 1
+                except IndexError:  # out of bounds
+                    break  # now
 
-
+    # if the code has advanced to here, then there was no win
+    return False, looking_for
 

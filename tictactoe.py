3238b3e47f881a03e958bda86a0b9275e7267d75
Muxite <muk.chunpongtong@gmail.com>
2024-06-04 17:46:43 -0600

    win_check works with efficiency
    win_check can check for wins. Benchmark time is 0.01433ms for a 3x3 board. (100,000 samples)

--- tictactoe.py
+++ tictactoe.py
@@ -1,11 +1,17 @@
 import random
-
+import time
 board_width, board_height = 3, 3
 score_to_win = 3
-board = []
+
+
+def bound_check(to_check, max_x, max_y):
+    if to_check[0] < 0 or to_check[1] < 0 or to_check[0] > max_x or to_check[1] > max_y:
+        return False
+    return True
 
 
 def win_check(board_w, board_h, test_board, updated_square, required_score):
+    start = time.time_ns()
     looking_for = test_board[updated_square[1]][updated_square[0]]
     neighbors = []
     # get a list of neighbors of starting square
@@ -27,8 +33,6 @@ def win_check(board_w, board_h, test_board, updated_square, required_score):
                 if test_board[try_y][try_x] == looking_for:
                     neighbors.append((try_x, try_y))
 
-    print(neighbors)
-
     for neighbor in neighbors:
         vector = (neighbor[0]-updated_square[0], neighbor[1]-updated_square[1])  # ex (1,0), (-1,-1)
         squares = [updated_square]
@@ -37,41 +41,34 @@ def win_check(board_w, board_h, test_board, updated_square, required_score):
         reversing = False
         i = 0
         while i < required_score*2:
-            if current_score > required_score:
+            if current_score >= required_score:
                 print("WINNER" + str(looking_for))
                 print(squares)
-                return True, looking_for  # did someone win? if so, who?
+                return True, looking_for, time.time_ns() - start  # did someone win? if so, who? how long?
             else:
-                # if the next square in the vector is same as looking_for
-                try:  # 2 is added to skip to after neighbor
-                    if not reversing:
-                        sam = updated_square[0] + (i + 1) * vector[0], updated_square[1] + (i + 1) * vector[1]
-                    else:
-                        sam = updated_square[0] + (i + 1) * -vector[0], updated_square[1] + (i + 1) * -vector[1]
+                if not reversing:
+                    x = updated_square[0] + (i+1) * vector[0]
+                    y = updated_square[1] + (i+1) * vector[1]
+                    sam = x, y
 
-                    if test_board[sam[1]][sam[0]] == looking_for:
-                        current_score += 1
-                        squares.append(sam)
-                    else:
-                        if not reversing:
-                            print("reversing")
-                            reversing = True
-                            i *= 0
-                        else:
-                            print("ended")
-                            break
-                except IndexError:  # out of bounds
+                else:
+                    x = updated_square[0] + (i+1) * -vector[0]
+                    y = updated_square[1] + (i+1) * -vector[1]
+                    sam = x, y
+
+                if not bound_check(sam, board_width-1, board_height-1) or test_board[sam[1]][sam[0]] != looking_for:
                     if not reversing:
                         reversing = True
-                        i *= 0
+                        i = i * 0
                     else:
                         break
-                    print("out of bounds")
-                    break  # now hit edge
-
+                else:
+                    current_score += 1
+                    squares.append(sam)
+            i += 1
 
     # if the code has advanced to here, then there was no win
-    return False, looking_for
+    return False, looking_for, time.time_ns() - start
 
 
 def display_board(to_display):
@@ -79,8 +76,25 @@ def display_board(to_display):
         print(to_display[i])
 
 
+def bot(foresight, board_w, board_h, board, team, enemy, required_score):
+    # literally simulate a game in its head
+    # find all empties, simulate what happens.
+    empties = []
+    scores = []  # scores will be assigned to each empty
+    for row in len(board):
+        for column in len(board[row]):
+            if board[row][column] == 0:
+                empties.append((column, row))  # lists go (y, x)
+                scores.append(0)
+
+    for empty in empties:
+        b = board.copy()
+        b[empty[1]][empty[0]] = team
+        # first pass, can we win next round?
+        win_check(board_w, board_h, b, empty, required_score)
+
+
 def play():
-    global board
     board = []
     for a in range(board_height):
         sub = []
@@ -91,7 +105,7 @@ def play():
     played_squares = []
     # as many turns as there are squares
     first_player = random.randint(0, 1)  # if 0, the human goes first
-    for turns in range(board_width*board_height):
+    for turns in range(board_width*board_height*2):
         if turns % 2 == first_player:
             # human plays
             while True:
@@ -109,5 +123,19 @@ def play():
             # bot plays
             pass
 
-play()
 
+# play()
+
+def benchmark_win_check(reps):  # roughly 0.01433ms
+    board = [[0, 1, 1],
+             [0, 1, 1],
+             [1, 0, 1]]
+
+    adder = 0
+    for i in range(reps):
+        adder += win_check(board_width, board_height, board, (1, 1), score_to_win)[2]  # times
+    ave_time = adder/reps
+    print(str(ave_time/1000000) + "ms")
+
+
+benchmark_win_check(100000)

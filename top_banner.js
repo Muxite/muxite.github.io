7341274197535fad4cd925ad99c09b47efbbb75f
Muxite <muk.chunpongtong@gmail.com>
2024-06-20 20:48:15 -0600

    added good animated banner
    needs to be more legible

---
+++ top_banner.js
@@ -0,0 +1,24 @@
+var title = document.querySelector('.top h1');
+var webname = document.querySelector('.top h2');
+
+function titleChange(){
+    var width = innerWidth;
+    console.log(width);
+    if (width > 1280){
+        title.style.fontSize = 0.006*width + 'em';
+        webname.style.fontSize = 0.003*width + 'em';
+        console.log(title.style.fontSize);
+    }
+    else{
+        title.style.fontSize = 0.006*1280 + 'em';
+        webname.style.fontSize = 0.003*1280 + 'em';
+        console.log(title.style.fontSize);
+    }
+}
+
+// Event listener for window resize
+window.addEventListener('resize', titleChange);
+
+window.onload = function() {
+    titleChange();
+};

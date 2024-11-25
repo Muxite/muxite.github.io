fd04e1dd3cc20a460a04fc5164d3178a680f040b
Muk Chunpongtong <muk.chunpongtong@gmail.com>
2024-11-25 07:18:44 -0800

    voltage colors v2

--- scripts/voltage_color.js
+++ scripts/voltage_color.js
@@ -7,8 +7,8 @@ document.addEventListener("DOMContentLoaded", function () {
     function sigmoid(x) {
         return 1 / (1 + Math.exp(-x));
     }
-
-    function voltageToColor(voltage) {
+    
+    function voltageToColorOld(voltage) {
         var high = 24.4;
         var low = 22.2;
         
@@ -37,6 +37,52 @@ document.addEventListener("DOMContentLoaded", function () {
         redText.textContent = Math.round(r*10)/10;
         greenText.textContent = Math.round(g*10)/10;
     }
+
+    function sigmoidInterpolate(val, x1, y1, x2, y2) {
+        var xc = (x1 + x2) / 2; // center point
+        var k = 10 / (x2 - x1); // steepness
+        return y1 + (y2 - y1) / (1 + Math.exp(-k * (val - xc)));
+    }
+    
+    function voltageToColor(voltage) {
+        var high = 24.4;
+        var low = 22.2;
+        var yellow = (high + low)/2
+        var red = 0;
+        var green = 255;
+
+        if (voltage <= low)
+        {
+            red = 255;
+            green = 0;
+        }
+        else if (low < voltage && voltage <= yellow)
+        {
+            red = 255;
+            green = sigmoidInterpolate(voltage, low, 0, yellow, 255);
+        }
+        else if (yellow < voltage && voltage <= high)
+        {
+            red = sigmoidInterpolate(voltage, yellow, 255, high, 0);
+            green = 255;
+        }
+        else
+        {
+            green = 255;
+            red = 0;
+        }
+
+
+        var r = red;
+        var g = green;
+
+        var b = 0;
+        sliderText.textContent = `voltage: ${voltage}V`;
+        output.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+        redText.textContent = Math.round(r*10)/10;
+        greenText.textContent = Math.round(g*10)/10;
+    }
+
 
     function updateColor() {
         var voltage = slider.value/10;
